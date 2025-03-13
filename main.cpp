cd3aece731e63f5905a854b2282499bbaa71d065
Matteo Terzi <support@luaminha.com>
2025-03-13 18:32:12 +0100

    Refactor menu handling by moving logic to start_menu function and simplifying main function

--- main.cpp
+++ main.cpp
@@ -4,27 +4,9 @@
 int main() {
     initialize_ncurses();
 
-    int startx, starty, width, height;
-    height = 10;
-    width = 30;
-    starty = (LINES - height) / 2; // Calculate for a center placement
-    startx = (COLS - width) / 2;   // Calculate for a center placement
-    WINDOW *menu_win = create_menu_window(height, width, starty, startx);
+    start_menu();
 
-    const char *choices[] = {
-        "Gioca",
-        "Classifica",
-        "Livelli",
-        "Exit",
-    };
-    int n_choices = sizeof(choices) / sizeof(char *);
-
-    int choice = handle_user_input(menu_win, choices, n_choices);
-
-    mvprintw(0, 0, "You chose choice %d with choice string %s\n", choice, choices[choice - 1]);
-    clrtoeol();
-    refresh();
-    getch();
-    endwin();             // End ncurses
+    endwin();  
+    
     return 0;
 }
--- menu_handler/menu.cpp
+++ menu_handler/menu.cpp
@@ -67,3 +67,32 @@ int handle_user_input(WINDOW *menu_win, const char *choices[], int n_choices) {
     }
     return choice;
 }
+
+
+void start_menu(){
+    
+
+    int startx, starty, width, height;
+    height = 10;
+    width = 30;
+    starty = (LINES - height) / 2; // Calculate for a center placement
+    startx = (COLS - width) / 2;   // Calculate for a center placement
+    WINDOW *menu_win = create_menu_window(height, width, starty, startx);
+
+    const char *choices[] = {
+        "Gioca",
+        "Classifica",
+        "Livelli",
+        "Exit",
+    };
+    int n_choices = sizeof(choices) / sizeof(char *);
+
+    int choice = handle_user_input(menu_win, choices, n_choices);
+
+    mvprintw(0, 0, "You chose choice %d with choice string %s\n", choice, choices[choice - 1]);
+    clrtoeol();
+    refresh();
+    getch();
+    // End ncurses
+
+}
