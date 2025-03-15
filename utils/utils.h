648c6f2e7326bb2555e6c04415334a2529984c7d
lluaminha <matteoterzi01@gmail.com>
2025-03-15 11:54:51 +0100

    Update utils.h

--- utils/utils.h
+++ utils/utils.h
@@ -1,6 +1,6 @@
 
 #include "../includes/include.h"
-
+// same here fiocane
 class Utils {
 public:
     static WINDOW* CreateBoxWindowCentered(WINDOW* boxForFile, int heightDivisor, int widthDivisor);
