8d85e87ab6338befdc060249ae29007f4a322a79
ThatsLeo <leonardorocco.monaco@studio.unibo.it>
2025-03-17 17:58:59 +0100

    Classe serpente implementata, ma non completa.

--- Serpente.cpp
+++ Serpente.cpp
@@ -1,4 +1,25 @@
+#include "main.h"
 
+int main(int argc, char ** argv){
+
+    initscr();
+    int xMax, yMax;
+    
+    WINDOW * win;
+    win = newwin(11, 25, 5, 30);
+    getmaxyx(win, yMax, xMax);
+    refresh();
+    win = newwin(11, 25, yMax, xMax);
+    box(win, 0, 0);
+    
+    wrefresh(win);
+    
+    getch();
+    getch();
+
+    endwin();
+    return 0;
+}
 
 
 
