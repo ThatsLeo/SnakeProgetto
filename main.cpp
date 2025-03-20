8474abeec6e515532f0977a511420643a7a85ba8
Matteo Terzi <support@luaminha.com>
2025-03-20 21:02:15 +0100

    Remove unused file reading and window display logic from main function

--- main.cpp
+++ main.cpp
@@ -7,30 +7,9 @@ int main() {
     FileManager fileManager = FileManager();
     Menu menu = Menu();
 
-
     fileManager.writeFile("Hello World\n");
     fileManager.writeFileAppend("Appeso \n");
     
-    const int bufferSize = 1024; // massimo 1024 caratteri compreso null-terminator
-    char buffer[bufferSize];
-    int chNumber = fileManager.readFile(buffer, bufferSize);
-    mvprintw(0, 0, "Press enter to start (s) also this is whats in the save file:\n \n");
-
-    WINDOW * boxForFile;
-    boxForFile = Utils::CreateBoxWindowCentered(boxForFile, 5, 2); 
-    mvwprintw(boxForFile, 2, 3, ("%s", buffer) );
-    wrefresh(boxForFile); // refresh window in this case the box 
-
-
-    
-    WINDOW * insideBox;
-    insideBox = Utils::CreateTextBox(insideBox, chNumber, 6, 4); 
-    mvwprintw(insideBox, 1, 1, ("%s", buffer) );
-    wrefresh(insideBox); 
-
-    refresh(); // refresh the screen(console whole screen i guess idk porcodidddidoooooo)
-
-    getch();
     clear();
     refresh();
 
