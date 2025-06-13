d2b6c9cd02b88384fa30245dcca956791e1c6930
Matteo Terzi <support@luaminha.com>
2025-06-13 22:28:22 +0200

    Refactor menu handling by removing commented code and unnecessary variables in main_menu.cpp; update .gitignore to include .txt and .tex files.

--- menu_handler/main_menu.cpp
+++ menu_handler/main_menu.cpp
@@ -229,12 +229,9 @@ void Menu::start_menu() {
         wclear(menu_win); 
         wrefresh(menu_win); 
 
-        int choice = handle_user_input(); // usate la scelta
-        // Fate qualcosa con la scelta per esempio se fai gioca (1)
-        // aggiungete la vostra classe con la griglia e snake etc.
+        int choice = handle_user_input(); 
         old_choice = choice; // Salva la scelta precedente per il refresh del menu
 
-        // Ex. if (choice == 1) { Game game = Game(); game.start_game(); } 
         if (choice == 1) {   // Usiamo uno switch per gestire le scelte appena le abbiamo tutte
             game_state = start_game();
             gameOver(game_state);
@@ -260,22 +257,7 @@ void Menu::start_menu() {
             }
             
             wrefresh(insideBox);
-            // wclear(menu_win);
-            // wrefresh(menu_win);
-            // levelMenu.PrintLevels();
-            // int c;
-            // while (true) {
-            //     c = wgetch(menu_win);
-            //     if (c == KEY_UP) {
-            //         levelMenu.prevLevel();
-            //     } else if (c == KEY_DOWN) {
-            //         levelMenu.nextLevel();
-            //     } else if (c == 10) { // Enter key
-            //         break; // Exit the loop to return to the menu
-            //     }
-            //     clear();
-            //     levelMenu.PrintLevels();
-            // }
+            
         }
          else if (choice == 4) {
             break;
